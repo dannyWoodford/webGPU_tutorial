173563f9ea0e02d1413c1972fa0cf64e980a9297
Danny Woodford <dwoodford@brandgage.com>
2023-07-15 21:58:42 -0400

    Up to 15 bind-groups-and-layouts

---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import { App } from "./control/app";
+import Stats from 'stats.js'
+
+const canvas : HTMLCanvasElement = <HTMLCanvasElement> document.getElementById("gfx-main");
+
+const app = new App(canvas);
+app.InitializeRenderer();
+app.run();
+
+
+var stats = new Stats();
+stats.showPanel( 0 ); // 0: fps, 1: ms, 2: mb, 3+: custom
+stats.dom.style.position = "absolute"
+document.getElementById("webgpu-container")?.appendChild( stats.dom );
+
+function animate() {
+
+	stats.begin();
+
+	// monitored code goes here
+
+	stats.end();
+
+	requestAnimationFrame( animate );
+
+}
+
+requestAnimationFrame( animate );
+
